fc5556f5a496786e7b80092c235c643929a12433
Lina <linaoeu567556@gmail.com>
2024-08-13 20:47:04 +0700

    Not finished all task -;-

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 const quadratic = (a, b, c) => {
-    // complete the code here
+       // complete the code here
+       // Step 1: Calculate the Discriminant
+       const discriminant = b * b - 4 * a * c; 
+    
+       if (discriminant >= 0) { // Step 2: Check if roots exist
+           const x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+           const x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+           return x1, x2;
+       } else {
+           return "No root!";
+       }
 }
+console.log(quadratic(1, 2, 1)); // x1 = -1, x2 = -1
+console.log(quadratic(1, 1, 1)); // No root!
+
 
 const formatSum = n => {
     // complete the code here
+    
+    
 }
+
 
 
 
