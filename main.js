4156dfeb41a7098310cbf45cd7ae919c7f201800
Lina <linaoeu567556@gmail.com>
2024-08-14 19:08:09 +0700

    Resubmitted

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const quadratic = (a, b, c) => {
        if (discriminant >= 0) { // Step 2: Check if roots exist
            const x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
            const x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-           return x1, x2;
+        return { x1, x2 };
+        // return x1 = ${x1}, x2 = ${x2};
        } else {
            return "No root!";
        }
@@ -17,9 +18,15 @@ console.log(quadratic(1, 1, 1)); // No root!
 
 const formatSum = n => {
     // complete the code here
-    
+    if (n === 1) {
+        return '1'; // Base case
+    } else {
+        return formatSum(n - 1) + '+' + n; // Recursive case
+    }
     
 }
+console.log(formatSum(5)); // 1+2+3+4+5
+console.log(formatSum(10)); // 1+2+3+4+5+6+7+8+9+10
 
 
 
